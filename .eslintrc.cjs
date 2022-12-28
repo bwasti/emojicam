93b4620842115e7503457ac137db1aa2efe6f01b
Bram Wasti <bwasti@brams-air.myfiosgateway.com>
2022-12-28 10:05:13 -0500

    cleanup

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: false, // Allows for the parsing of JSX
+    },
+  },
+  plugins: ["simple-import-sort"],
+  ignorePatterns: ["dist", "global.d.ts"],
+  extends: [
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "no-unused-vars": "off",
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [["^\\u0000", "^@?\\w", "^[^.]", "^\\."]],
+      },
+    ],
+    "simple-import-sort/exports": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
--- model.ts
+++ model.ts
@@ -1,22 +1,21 @@
-import * as sm from '@shumai/shumai'
+import * as sm from "@shumai/shumai";
 
 export class EmojiClassifier extends sm.module.Module {
   constructor(num_emojis) {
-    super()
-    this.conv0 = sm.module.conv2d(3, 8, 3, {stride: 2})
-    this.conv1 = sm.module.conv2d(8, 32, 3, {stride: 2})
-    this.linear = sm.module.linear(32, num_emojis)
+    super();
+    this.conv0 = sm.module.conv2d(3, 8, 3, { stride: 2 });
+    this.conv1 = sm.module.conv2d(8, 32, 3, { stride: 2 });
+    this.linear = sm.module.linear(32, num_emojis);
   }
   forward(x) {
-    x = sm.avgPool2d(x,2,2,2,2)
-    x = this.conv0(x)
-    x = x.tanh()
-    x = sm.avgPool2d(x,2,2,2,2)
-    x = this.conv1(x)
-    x = x.tanh()
-    x = x.reshape([x.shape[0], 32])
-    x = this.linear(x)
-    return x.softmax(1)
+    x = sm.avgPool2d(x, 2, 2, 2, 2);
+    x = this.conv0(x);
+    x = x.tanh();
+    x = sm.avgPool2d(x, 2, 2, 2, 2);
+    x = this.conv1(x);
+    x = x.tanh();
+    x = x.reshape([x.shape[0], 32]);
+    x = this.linear(x);
+    return x.softmax(1);
   }
 }
-
