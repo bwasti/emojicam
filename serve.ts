49a033c1043c97df63193958bdef9bf4f89a53f0
Bram Wasti <bwasti@fb.com>
2022-12-28 00:37:15 -0500

    Update serve.ts

--- serve.ts
+++ serve.ts
@@ -7,28 +7,35 @@ import { serve, file } from "bun";
 
 const emoji = JSON.parse(Bun.readFile('emoji.json'))
 const model = new EmojiClassifier(emoji.length)
-model.checkpoint('weights', () => false)
+model.checkpoint('weights') // this loads all the weights
 
 serve({
   async fetch(req) {
     if (req.method === 'POST') {
       const response = [] 
       const data = new Uint8Array(await req.arrayBuffer())
-      sm.util.tidy(() => {
-        let t = sm.tensor(data).astype(sm.dtype.Float32)
-        // mask out the alpha channel
-        t = t.reshape([36 * 36, 48 * 36, 4]).eval().index([':', ':', ':3'])
-        // arrayfire only supports dim <= 4 :( so we have to hack
-        t = t.reshape([36, 36, 48, 36 * 3])
-        t = t.transpose([0, 2, 1, 3])
-        // only need 3 channels
-        t = t.reshape([36 * 48, 36, 36, 3])
-        t = t.transpose([0, 3, 1, 2]).div(sm.scalar(255))
-        const out = model(t).argmax(1)
-        for (let i of out.toInt32Array()) {
-          response.push(emoji[i])
-        }
-      })
+      
+      // convert data to a float tensor
+      let t = sm.tensor(data).astype(sm.dtype.Float32)
+
+      // mask out the alpha channel
+      t = t.reshape([36 * 36, 48 * 36, 4]).eval().index([':', ':', ':3'])
+
+      // arrayfire only supports dim <= 4 :( so we have to hack
+      t = t.reshape([36, 36, 48, 36 * 3])
+      t = t.transpose([0, 2, 1, 3])
+      t = t.reshape([36 * 48, 36, 36, 3])
+      t = t.transpose([0, 3, 1, 2]).div(sm.scalar(255))
+
+      // run the model
+      const out = model(t).argmax(1)
+
+      // convert indices back to emoji (strings)
+      for (let i of out.toInt32Array()) {
+        response.push(emoji[i])
+      }
+      
+      // all set to return!
       return new Response(JSON.stringify(response))
     }
     return new Response(file("./index.html"))
